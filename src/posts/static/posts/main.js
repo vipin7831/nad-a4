edd0871c1647edbcbfbcff46a22dfec09f9916e4
Vipin Saini <vsaini411.vs@gmail.com>
2024-04-05 23:24:23 -0400

    added spinner from bootstrap to show loading spinner before loading posts

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -2,6 +2,7 @@ console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
 
 $.ajax({
     type: 'GET',
@@ -22,12 +23,16 @@ $.ajax({
     success: function(response){
         console.log(response)
         const data = response.data
-        console.log(data)
-        data.forEach(el => {
-            postsBox.innerHTML += `
-                ${el.title} - </b>${el.body}</b> <br>
-            `
-        });
+        setTimeout(() => {
+            spinnerBox.style.display = 'none'
+            console.log(data)
+            data.forEach(el => {
+                postsBox.innerHTML += `
+                    ${el.title} - </b>${el.body}</b> <br>
+                `
+            });
+        }, 100)
+        
     },
     error: function(error){
         console.log(error)
