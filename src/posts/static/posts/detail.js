30bb1d56b5ec8f06bb5be5a4d9a8cf277c2d9231
Vipin Saini <vsaini411.vs@gmail.com>
2024-04-06 18:15:21 -0400

    added modals for update and delete buttons on details page

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -8,6 +8,9 @@ const deleteBtn = document.getElementById('delete-btn')
 const url = window.location.href + "data/"
 const spinnerBox = document.getElementById('spinner-box')
 
+const titleInput =  document.getElementById('id_title')
+const bodyInput = document.getElementById('id_body')
+
 backBtn.addEventListener('click', () => {
     history.back()
 })
@@ -40,6 +43,9 @@ $.ajax({
 
         postBox.appendChild(titleEl)
         postBox.appendChild(bodyEl)
+
+        titleInput.value = data.title
+        bodyInput.value = data.body
         
         spinnerBox.style.display = 'none'
     },
