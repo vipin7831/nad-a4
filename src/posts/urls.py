b6a0b94c45d36454e6c379b9b961a97a9a710fb1
Vipin Saini <vsaini411.vs@gmail.com>
2024-04-06 17:36:05 -0400

    added spinner to details page and got post data through ajax

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -4,6 +4,7 @@ from .views import (
     load_post_data_view,
     like_unlike_post,
     post_detail,
+    post_detail_data_view
 
 )
 
@@ -17,4 +18,5 @@ urlpatterns = [
 
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
 
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data')
 ]
