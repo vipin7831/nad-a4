6639b2af99a4f6560f4b686fd4870fb2ffd34fe0
Vipin Saini <vsaini411.vs@gmail.com>
2024-04-06 00:04:49 -0400

    added a button to load more posts

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,55 +1,65 @@
 console.log('hello world')
 
-const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
 const spinnerBox = document.getElementById('spinner-box')
+const loadBtn = document.getElementById('load-btn')
+const endBox = document.getElementById('end-box')
 
-$.ajax({
-    type: 'GET',
-    url: 'hello-world/',
-    success: function(response){
-        console.log('success', response.text)
-        helloWorldBox.textContent = response.text
-    },
-    error: function(error){
-        console.log('error', error)
-    }
+let visible = 3;
+
+const getData = () => {
+    $.ajax({
+        type: 'GET',
+        url: `/data/${visible}`,
+        success: function(response){
+            console.log(response)
+            const data = response.data
+            setTimeout(() => {
+                spinnerBox.style.display = 'none'
+                console.log(data)
+                data.forEach(el => {
+                    postsBox.innerHTML += `
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <h5 class="card-title">${el.title}</h5>
+                            <p class="card-text">${el.body}</p>                        
+                        </div>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Details</a>
+                                </div>
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Like</a>
+                                </div>
+                            </div>   
+                        </div>
+                    </div>
+                    `
+                });
+            }, 100)
+
+            console.log(response.size)
+
+            if(response.size === 0){
+                endBox.textContent = 'No posts added yet...'
+            }
+            else if(response.size <= visible){
+                loadBtn.style.display = 'none'
+                endBox.textContent = 'No more posts to load...'
+            }
+            
+        },
+        error: function(error){
+            console.log(error)
+        }
+    })
+}
+
+loadBtn.addEventListener('click', () => {
+    spinnerBox.style.display = 'block'
+    visible += 3
+    getData()
 })
 
-
-$.ajax({
-    type: 'GET',
-    url: '/data/',
-    success: function(response){
-        console.log(response)
-        const data = response.data
-        setTimeout(() => {
-            spinnerBox.style.display = 'none'
-            console.log(data)
-            data.forEach(el => {
-                postsBox.innerHTML += `
-                <div class="card mb-2">
-                    <div class="card-body">
-                        <h5 class="card-title">${el.title}</h5>
-                        <p class="card-text">${el.body}</p>                        
-                    </div>
-                    <div class="card-footer">
-                        <div class="row">
-                            <div class="col-1">
-                                <a href="#" class="btn btn-primary">Details</a>
-                            </div>
-                            <div class="col-1">
-                                <a href="#" class="btn btn-primary">Like</a>
-                            </div>
-                        </div>   
-                    </div>
-                </div>
-                `
-            });
-        }, 100)
-        
-    },
-    error: function(error){
-        console.log(error)
-    }
-})
+getData()
