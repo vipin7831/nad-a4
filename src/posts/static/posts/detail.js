7ed066a6bcb692b5e3f573cde64f4cac52534e2d
Vipin Saini <vsaini411.vs@gmail.com>
2024-04-06 17:57:33 -0400

    only viewing update and delete button on details page of post if the author who made the post is viewing the details

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -1,6 +1,9 @@
 console.log('hello world detail')
 
 const backBtn = document.getElementById('back-btn')
+const updateBtn = document.getElementById('update-btn')
+const deleteBtn = document.getElementById('delete-btn')
+
 const url = window.location.href + "data/"
 const spinnerBox = document.getElementById('spinner-box')
 
@@ -14,6 +17,17 @@ $.ajax({
     url: url,
     success: function(response){
         console.log(response)
+
+        const data = response.data
+
+        if(data.logged_in !== data.author){
+            console.log('different')
+        } else { 
+            console.log('same')
+            updateBtn.style.display = "block"
+            deleteBtn.style.display = 'block'
+        }
+
         spinnerBox.style.display = 'none'
     },
     error: function(error){
