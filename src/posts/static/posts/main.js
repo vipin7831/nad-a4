5afd3e44f0643d5a96f13a666e5fb00806a97285
Vipin Saini <vsaini411.vs@gmail.com>
2024-04-05 22:49:47 -0400

    added ajax call for DOM manipulation

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -2,4 +2,15 @@ console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
 
-helloWorldBox.innerHTML = "hello <b>world</b>"
+
+$.ajax({
+    type: 'GET',
+    url: 'hello-world/',
+    success: function(response){
+        console.log('success', response.text)
+        helloWorldBox.textContent = response.text
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
