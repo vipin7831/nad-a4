527bf39fb8c288bff3750f355d349c803cac928a
Vipin Saini <vsaini411.vs@gmail.com>
2024-04-06 20:32:40 -0400

    added user profile view

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Profile
+from .forms import ProfileForm
+from django.http import JsonResponse
 
 # Create your views here.
+
+def my_profile_view(request):
+    obj = Profile.objects.get(user=request.user)
+    form = ProfileForm(request.POST or None, request.FILES or None, instance=obj)
+
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        is_ajax = True
+    else:
+        is_ajax = False
+
+    if is_ajax == True:
+        if form.is_valid():
+            instance = form.save()
+            return JsonResponse({
+                'bio': instance.bio,
+                'avatar': instance.avatar.url,
+                'user': instance.user.username
+            })
+    
+    context = {
+        'obj': obj,
+        'form': form,
+    }
+
+    return render(request, 'profiles/main.html', context)
